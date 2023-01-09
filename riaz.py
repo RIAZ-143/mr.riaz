74a7d9256d50d0d5f8786a6bcbc65fc770b0b3d3
MR.RIAZ <109266957+RIAZ-143@users.noreply.github.com>
2023-01-09 14:30:29 +0500

    Update riaz.py

--- riaz.py
+++ riaz.py
@@ -1,2 +1,22 @@
-import RIAZ
-RIAZ.follow()
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+os.system('git pull')
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import Riaz
+
